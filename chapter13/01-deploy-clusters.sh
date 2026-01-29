a1f56da83bc2b26aeffebebedb8336bdc3b430fe
Nico Vibert <nvibert@cisco.com>
2026-01-29 14:18:28 +0000

    fixed wrong chapter

--- chapter13/01-deploy-clusters.sh
+++ chapter13/01-deploy-clusters.sh
@@ -3,11 +3,11 @@ set -euxo pipefail
 
 kind create cluster \
   --config kind.yaml \
-  --name chapter12a
+  --name chapter13
 
 helm upgrade --install \
   cilium cilium/cilium \
-  --kube-context kind-chapter12a \
+  --kube-context kind-chapter13 \
   --namespace kube-system \
   --version "1.18.1" \
   --set "hubble.relay.enabled=true"
