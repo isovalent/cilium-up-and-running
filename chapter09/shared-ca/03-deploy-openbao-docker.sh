ff9deedcf0f21ddba74e061edaac18f24c9258b8
James Laverack <james@laverack.dev>
2026-02-12 19:13:58 +0000

    Remove unused example from Chapter 9

--- chapter09/shared-ca/03-deploy-openbao-docker.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euxo pipefail
-
-docker run \
-  --detach \
-  --name openbao \
-  --publish 8200:8200 \
-  --env VAULT_ADDR='http://0.0.0.0:8200' \
-  'quay.io/openbao/openbao:2.4.1'
--- chapter09/shared-ca/03-deploy-openbao.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -euxo pipefail
-
-podman run \
-  --detach \
-  --name openbao \
-  --env VAULT_ADDR='http://0.0.0.0:8200' \
- 'quay.io/openbao/openbao:2.4.1'
